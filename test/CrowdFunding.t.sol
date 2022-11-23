643ff3e5dae491dd4750186e5087d291373ed000
Addis Belete <addis.bel@gmail.com>
2022-11-23 10:54:04 +0300

    Add more test

--- test/CrowdFunding.t.sol
+++ test/CrowdFunding.t.sol
@@ -2,10 +2,10 @@
 pragma solidity 0.8.7;
 
 import "../lib/forge-std/src/test.sol";
-import "../src/CrowdFunding.sol";
+import "../src/Crowdfunding.sol";
 
-contract CrowdFundingTest is Test {
-    CrowdFunding crowdfunding;
+contract CounterTest is Test {
+    CrowdFunding public crowdfunding;
 
     function setUp() public {
         crowdfunding = new CrowdFunding();
@@ -17,5 +17,72 @@ contract CrowdFundingTest is Test {
         crowdfunding.setNewCampaign("Addis", 1, fund, 10);
         assertEq(crowdfunding.campaignId(), 1);
         vm.stopPrank();
+    }
+
+    function testFundCampaign() public {
+        testSetNewCampaign();
+        hoax(address(20));
+        crowdfunding.fundCampaign{value: 2 ether}(1);
+        vm.stopPrank();
+        hoax(address(30));
+        crowdfunding.fundCampaign{value: 2 ether}(1);
+        vm.stopPrank();
+    }
+
+    function testFailFundCampaign() public {
+        testSetNewCampaign();
+        hoax(address(20));
+        crowdfunding.fundCampaign{value: 2 ether}(2);
+        vm.stopPrank();
+        hoax(address(30));
+        crowdfunding.fundCampaign{value: 2 ether}(2);
+        vm.stopPrank();
+    }
+
+    function testFailFundCampaign1() public {
+        testFundCampaign();
+        hoax(address(50));
+        uint256 time = 11 * 24 * 60 * 60;
+        skip(time);
+        crowdfunding.fundCampaign{value: 2 ether}(1);
+        vm.stopPrank();
+    }
+
+    function testWithdrawFund() public {
+        testFundCampaign();
+        address owner = crowdfunding.getCampaignOwner(1);
+        uint256 time = 11 * 24 * 60 * 60;
+        vm.startPrank(owner);
+        skip(time);
+        crowdfunding.withdrawFund(1);
+        uint256 totalFund = crowdfunding.getTotalCampianFund(1);
+        console.log(totalFund);
+        assertEq(totalFund, 0);
+    }
+
+    function testFailWithdrawFund() public {
+        testFundCampaign();
+        vm.startPrank(address(10));
+        crowdfunding.withdrawFund(1);
+    }
+
+    function testFailWithdrawFund1() public {
+        testFundCampaign();
+        vm.startPrank(address(12));
+        crowdfunding.withdrawFund(1);
+    }
+
+    function testCancelCampian() public {
+        testFundCampaign();
+        hoax(address(10));
+        crowdfunding.cancelCampian(1);
+        uint256 totalFund = crowdfunding.getTotalCampianFund(1);
+        console.log("total campian fund after campaign canceled ", totalFund);
+    }
+
+    function testFailCancelCampaign() public {
+        testFundCampaign();
+        vm.startPrank(address(20));
+        crowdfunding.cancelCampian(1);
     }
 }
